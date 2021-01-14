86cdda706c6e942d734bb5056ebf96c5abd4c8cc
tootsraghani <tuttiraghani0@gmail.com>
2021-01-14 09:47:09 +0800

    1 file modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -10,14 +10,47 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   height: number;
   weight: number;
-  bmi: number;
-  bmiMessage: string;
+  age: number;
+  activityLevel: string;
+  actVal: number;
+  gender: string;
+  calorieCount: string;
+  calorie: number;
 
 constructor(public navCtrl: NavController) {}
 
-calculateBMI() {
-  this.bmi = this.weight / (this.height * this.height);
-  this.bmi = parseFloat(this.bmi.toFixed(2));
+calculateActivity(){
+  if (this.activityLevel = "Some or no activity"){
+    this.actVal = 1.2
+  }
+  else if (this.activityLevel = "Exercise 1-2 times a week"){
+    this.actVal = 1.4
+  }
+  else if (this.activityLevel = "Exercise 2-3 times a week"){
+    this.actVal = 1.6
+  }
+  else if (this.activityLevel = "Exercise 4-5 times a week"){
+    this.actVal = 1.75
+  }
+  else if (this.activityLevel = "Exercise 6-7 times a week"){
+    this.actVal = 2
+  }
+  else if (this.activityLevel = "Professional Athlete"){
+    this.actVal = 2.3
+  }
+  else{
+    this.actVal = "invalid"
+  }
+}
+calculateCalories() {
+if (this.gender = "m"){
+  this.calorie = (((10*this.weight) + (6.25*this.height) - (5*this.age )) + 5) * this.actVal;
+  this.calorie = parseFloat(this.calorie.toFixed(2));
+  this.calorieCount = "The right amount of calories for you is: this.calorie" 
+}
+else if (this.gender = "f"){
+  this.calorie = (((10*this.weight) + (6.25*this.height) - (5*this.age )) + 5) * this.actVal;
+}
 
   if (this.bmi < 18.5) {
     this.bmiMessage = "Underweight";
